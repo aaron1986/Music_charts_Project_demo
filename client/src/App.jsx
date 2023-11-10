58f352498d92c4d9bd9aa18e5ad43db3884af86b
aaron1986 <aaronbrucesmith86@gmail.com>
2023-11-10 13:07:33 +0000

    updated header and footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './assets/components/Header'
+import Footer from './assets/components/Footer'
 
 function App() {
   
@@ -8,6 +10,9 @@ function App() {
   return (
     <>
       <h1>Testing the app.jsx</h1>
+      <Header />
+
+      <Footer />
     </>
   )
 }
