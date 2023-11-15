e30fc7d5ac10fd0ad2f6a9a4ea07b5df17c1ac22
Paddy <pchonacki1@proton.me>
2023-11-15 14:10:47 +0000

    audio added

--- client/src/assets/components/About.jsx
+++ client/src/assets/components/About.jsx
@@ -2,41 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import video from "../animation.mp4";
+import paddy from "../paddy.jpg";
 
 export default function About() {
   return (
     <>
       <div className="container">
-        <video className="videoTag" autoPlay loop muted>
+        <video className="videoTag" autoPlay loop>
           <source src={video} type="video/mp4" />
         </video>
-        <div className="about-heading">
-          <div className="about-section">
-            <p className="about-text">
-              So, picture this: I'm this total coding newbie, right? Never
-              touched the stuff before. But then, I decide to take the plunge
-              into the coding world. Along the way, I meet these cool people –
-              tech wizards, problem-solving champs – you name it.
-              <br />
-              Their stories about coding adventures, both direct and indirect,
-              really got me hooked. Suddenly, I'm deciphering algorithms and
-              dancing through the binary maze. It's a wild ride.
-              <br />
-              Being around fellow learners feels like finding my tribe. Even
-              though I'm taking it slow, I've come to realize it's my own pace
-              of soaking up this coding thing.
-              <br />
-              Inspired by the tales of others who've walked this path, I find my
-              rhythm in learning. Armed with determination, I'm crafting my own
-              code story, adding lines and creating a digital masterpiece, one
-              step at a time.
-            </p>{" "}
-            {/* end of about-text */}
-            <img className="soup-image" src=""></img>
-          </div>{" "}
-          {/* end of about-section */}
-        </div>{" "}
-        {/* end of about-heading */}
+        <div className="about-heading"></div> {/* end of about-heading */}
         <div className="aaron-text">
           <img className="soup-image2" src=""></img>
           <p className="aaron">
@@ -54,6 +29,30 @@ export default function About() {
           {/* end of aaron */}
         </div>{" "}
         {/* end aaron-text */}
+        <div className="about-section">
+          <p className="about-text">
+            So, picture this: I'm this total coding newbie, right? Never touched
+            the stuff before. But then, I decide to take the plunge into the
+            coding world. Along the way, I meet these cool people – tech
+            wizards, problem-solving champs – you name it.
+            <br />
+            Their stories about coding adventures, both direct and indirect,
+            really got me hooked. Suddenly, I'm deciphering algorithms and
+            dancing through the binary maze. It's a wild ride.
+            <br />
+            Being around fellow learners feels like finding my tribe. Even
+            though I'm taking it slow, I've come to realize it's my own pace of
+            soaking up this coding thing.
+            <br />
+            Inspired by the tales of others who've walked this path, I find my
+            rhythm in learning. Armed with determination, I'm crafting my own
+            code story, adding lines and creating a digital masterpiece, one
+            step at a time.
+          </p>{" "}
+          {/* end of about-text */}
+          <img className="soup-image" src={paddy}></img>
+        </div>{" "}
+        {/* end of about-section */}
       </div>{" "}
       {/* end of container div */}
     </>
